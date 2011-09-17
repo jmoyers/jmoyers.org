c799e713471718e49fe1d5485220b9099d34a5d6
Joshua Moyers <jmoyers@gmail.com>
2011-09-16 19:42:24 -0700

    add logging

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express.createServer();
 
 app.configure(function(){
+   app.use(express.logger());
    app.use(express.static(__dirname + '/public'));
 });
 
