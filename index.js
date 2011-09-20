cc4dffc0ac31040a4803850001313e403e043626
Joshua Moyers <jmoyers@gmail.com>
2011-09-19 18:50:48 -0700

    switch to semi dynamic site

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 var express = require('express');
-
+var fs = require('fs');
+var path = require('path');
 var app = express.createServer();
 
 app.configure(function(){
+   app.register('ejs', require('ejs'));
+   app.register('html', require('ejs'));
    app.use(express.logger());
    app.use(express.static(__dirname + '/public'));
+});
+
+function urlify(string){                                               
+   return string.replace(/ /g, '-')                                    
+      .replace(/[^a-zA-Z\-]+/g, '')                                    
+      .toLowerCase();                                                  
+} 
+
+fs.readdir('views', function(err, files){
+   files.forEach(function(file){
+      var view = urlify(file.substring(0, file.indexOf('.')));
+      app.get('/' + view, function(req, res){
+         res.render(file, {
+            layout: 'layout/default.ejs'
+         });
+      });
+   });
 });
 
 app.listen(8080)
