a9d09d0df93ba0274866b5e03c989bbb071a7c7b
Joshua Moyers <jmoyers@gmail.com>
2013-02-18 14:59:51 -0800

    dev env

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    less: {
+      production: {
+        options: {
+          compress: true
+        },
+        files: {
+          "public/css/main.css": "src/less/main.less"
+        }
+      }
+    },
+    jshint: {
+      files: ['gruntfile.js', 'src/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>', 'src/less/*'],
+      tasks: ['jshint', 'less']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.registerTask('default', ['jshint', 'less']);
+
+};
