8c07bb4a4f24234b049bf5070df85ac98664cd51
Joshua Moyers <jmoyers@gmail.com>
2014-04-27 16:00:05 -0700

    Start nav, kill some images, compress others
    * Hack in a nav so that we can have a blog section
    * Does nothing yet (no posts to draw from)
    * Take out all the float stuff and re-order nav with push/pull
    * Compress all the pngs with a tool
    * Kill some unused icons
    * Kill grunt-contrib-watch because its the crappiest
    * Bring down Open Sans fonts and css locally
    * Include google analytics directly
    * Import github and solarized styling instead of concat

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,31 +11,29 @@ module.exports = function(grunt) {
         },
         files: {
           "public/style/main.css": [
-            "src/components/bootstrap/less/bootstrap.less",
-            "src/source/github.css",
-            "src/source/solarized.css",
             "src/main.less"
           ]
         }
       }
     },
     uglify: {
-      options: {
-        mangle: false
-      },
       production: {
         files: {
           "public/javascript/client.js": [
             "src/components/jquery/dist/jquery.js",
             "src/components/bootstrap/js/transition.js",
             "src/components/bootstrap/js/carousel.js",
-            "src/components/source/highlight.js"
+            "src/components/source/highlight.js",
+            "src/components/google/ga.js"
           ]
         }
       }
     },
     jshint: {
-      files: ['gruntfile.js', 'main.js'],
+      files: [
+        'gruntfile.js',
+        'main.js'
+      ],
       options: {
         node: true,
         globals: {
@@ -45,16 +43,11 @@ module.exports = function(grunt) {
           document: true
         }
       }
-    },
-    watch: {
-      files: ['<%= jshint.files %>', 'src/style/*.less', 'src/style/*.css'],
-      tasks: ['jshint', 'less', 'uglify']
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.registerTask('default', ['jshint', 'uglify', 'less']);
