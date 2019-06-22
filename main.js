39ce4740d8b429027c493c19f24d0c95ebea63b6
root <root@jmoyers.org>
2019-06-21 20:08:42 -0400

    Looks like i added compression

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 var express = require('express');
+var proxy = require('http-proxy-middleware');
 var app = express();
 var compress = require('compression')();
 
+app.use("/resume", express.static(__dirname + '/public'));
+app.use("/compilers", express.static(__dirname + '/public/compilers'));
+
+app.use("/", proxy({
+  target: 'http://198.199.110.247:2367',
+}));
+
 app.use(compress);
-app.use(express.static(__dirname + '/public'));
 
 app.listen(8080);
